8c0bad13a9042812a493dc4e3f239c4d9cac137c
ridel1e <ridel1e@yahoo.com>
2016-11-19 20:21:49 +0300

    server rewrote to vanilla js

--- index.js
+++ index.js
@@ -18,19 +18,3 @@ server.get('*', function (req, res) {
 server.listen(server.get('port'), function () {
   console.log('server listening on port' + server.get('port'));
 });
-
-// import express from 'express';
-//
-// const server = express();
-//
-// const port =  process.env.PORT || 5000;
-// server.set('port', port);
-//
-// server.use(express.static(`${__dirname}/public`));
-//
-// server.get('*', (req, res) =>
-//   res.sendFile(`${__dirname}/public/index.html`)
-// );
-//
-// server.listen(server.get('port'), () =>
-//   console.log(`server listening on port ${server.get('port')}`));
