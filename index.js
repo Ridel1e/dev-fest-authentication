18e174ffa25f2374c157f924bc625ad00f01b15b
ridel1e <ridel1e@yahoo.com>
2016-11-19 20:11:12 +0300

    server fix

--- index.js
+++ index.js
@@ -2,18 +2,35 @@
  * Created by ruslansalahov on 18/11/2016.
  */
 
-import express from 'express';
+const express = require('express');
 
 const server = express();
 
-const port =  process.env.PORT || 5000;
+const  port = process.env.Port || 5000;
 server.set('port', port);
 
-server.use(express.static(`${__dirname}/public`));
+server.use(express.static(__dirname + '/public'));
 
-server.get('*', (req, res) =>
-  res.sendFile(`${__dirname}/public/index.html`)
-);
+server.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-server.listen(server.get('port'), () =>
-  console.log(`server listening on port ${server.get('port')}`));
+server.listen(server.get('port'), function () {
+  console.log('server listening on port' + server.get('port'));
+});
+
+// import express from 'express';
+//
+// const server = express();
+//
+// const port =  process.env.PORT || 5000;
+// server.set('port', port);
+//
+// server.use(express.static(`${__dirname}/public`));
+//
+// server.get('*', (req, res) =>
+//   res.sendFile(`${__dirname}/public/index.html`)
+// );
+//
+// server.listen(server.get('port'), () =>
+//   console.log(`server listening on port ${server.get('port')}`));
